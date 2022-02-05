b3146ef1c1615371ef0a058062b0d7866c9bf4a7
Joe Moceri <joseph.thomas.moceri@gmail.com>
2022-02-05 14:50:10 -0800

    adding router, lazy loading and reorganizing

---
+++ src/app/App.js
@@ -0,0 +1,41 @@
+import './app.css';
+import React, { Suspense, lazy } from "react";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
+
+const VideoLoopTool = lazy(() => import('video-loop-tool/video-loop-tool.component'));
+const Playlist = lazy(() => import('playlist/playlist.component'));
+
+function App() {
+    return (
+        <Router>
+            <Suspense fallback={<div>Loading...</div>}>
+                <div>
+                    {/* Nav bar */}
+                    <ul>
+                        <li>
+                            <Link to="/video-loop-tool">Video Loop Tool</Link>
+                        </li>
+                        <li>
+                            <Link to="/playlist">Playlists</Link>
+                        </li>
+                    </ul>
+
+                    {/* Routes */}
+                    <Routes>
+                        <Route path="/playlist" element={<Playlist />}>
+                        </Route>
+                        <Route path="/video-loop-tool" element={<VideoLoopTool />}>
+                        </Route>
+                    </Routes>
+                </div>
+            </Suspense>
+        </Router>
+    );
+}
+
+export default App;
---
+++ src/playlist/playlist.component.js
@@ -0,0 +1,8 @@
+import React from 'react';
+require('jquery-ui/ui/widgets/slider');
+
+export default function Playlist() {
+    return (
+        <h2>Playlist Page</h2>
+    );
+};
